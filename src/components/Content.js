4406e31fe58579154b2b186b5f3c57528895ae1d
Junbro0708 <aktm708@naver.com>
2023-05-15 10:38:22 -0700

    making spa

---
+++ src/components/Content.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Content() {
+  return (
+    <div>
+      <h1>Content</h1>
+      <p>Welcome to the home page!</p>
+    </div>
+  );
+}
+
+export default Content;
---
+++ src/components/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function NotFound() {
+  return (
+    <div>
+      <h1>404</h1>
+      <p>Welcome to the home page!</p>
+    </div>
+  );
+}
+
+export default NotFound;
