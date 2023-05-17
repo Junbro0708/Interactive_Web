611c6bc9b3fd1f5ccc054a390311d877635ae31b
Junbro0708 <aktm708@naver.com>
2023-05-17 16:42:17 -0700

    applied animation in react

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { CSSTransition } from 'react-transition-group';
+import '../style/Content.css';
 
 function Content() {
+  const [isAnimating, setIsAnimating] = useState(false);
+
+  const handleClick = () => {
+    setIsAnimating(!isAnimating);
+  };
+  console.log(isAnimating)
   return (
     <div>
-      <h1>Content</h1>
-      <p>Welcome to the home page!</p>
+      <button onClick={handleClick}>Toggle Animation</button>
+      <CSSTransition
+        in={isAnimating}
+        timeout={300}
+        classNames="fade"
+        unmountOnExit
+      >
+        <div className="box">Animated Content</div>
+      </CSSTransition>
     </div>
   );
 }
