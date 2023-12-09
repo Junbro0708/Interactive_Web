d2c0407450f175188c5dd079f5c230c3960463b5
Junbro0708 <aktm708@naver.com>
2023-12-09 17:27:59 +0900

    changed text in test1 branch

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ function Home() {
       <ul>
         <li><Link to='/scrolling'>Parellax</Link></li>
         <li><Link to='/interact'>Interact</Link></li>
-        <li><Link to='/404'>Not Found</Link></li>
+        <li><Link to='/404'>Found</Link></li>
       </ul>
     </div>
   );
